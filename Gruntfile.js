5120c3fac97c8e91782d22965e42b955dce5e431
huangbilian <2650424700@qq.com>
2020-03-03 17:51:40 +0800

    htmlmin

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    htmlmin:{
+      options:{
+        removeComments:true,
+        collapseWhitespace:true,
+      },
+  files:{
+    src:'./index.html',
+    dest:'dist/index.html'
+  }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-htmlmin')
+
+  grunt.registerTask('default', ['htmlmin']);
+};
+
+
